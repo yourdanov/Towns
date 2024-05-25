8c0a823abd470e03686a7562bc3cc3d29cf72b0e
Iliana Chakarova <chaka@Iliana_DELL>
2024-05-25 15:17:38 +0200

    Implemented functionality to shuffle the existing towns

--- towns.js
+++ towns.js
@@ -2,6 +2,27 @@ $(document).ready(function() {
 	$('#btnDelete').click(deleteTown)
 });
 
+$(document).ready(function() {
+	$('#btnShuffle').click(shuffleTowns);
+});
+
+function shuffleTowns() {
+	let towns = $('#towns option').toArray();
+	$('#towns').empty();
+	shuffleArray(towns);
+	$('#towns').append(towns);
+	$('#result').text("Towns shuffled.");
+
+	function shuffleArray(array) {
+		for (var i = array.length - 1; i > 0; i--) {
+			var j = Math.floor(Math.random() * (i + 1));
+			var oldElement = array[i];
+			array[i] = array[j];
+			array[j] = oldElement;
+		}
+	}
+}
+
 function deleteTown() {
 	let townName = $('#townName').val();
 	$('#townName').val('');
